7a91210fcf32276bb20d9f2ca3dee14f32f7be37
root <itsupport@gibots.com>
2022-08-26 17:37:49 +0530

    26

--- addition_mul_num.py
+++ addition_mul_num.py
@@ -1,11 +1,12 @@
-def mul_int(num):
-    i=1
-    c=0
-    while i <= num:
-        c=c+i
-        i+=1
+def mul_int(n):
+    p=0
+    s=0
+    for i in range(1,n):
+        d=n**i
+        s=s+d
+    print("final scire",s)
 
 
 mul_int(5)
-
+    
 
